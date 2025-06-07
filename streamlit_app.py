aa3fb1052c4ea0947b80b69198a93bde2abd74e2
tcycuophpc <87329184+tcycuophpc@users.noreply.github.com>
2025-06-07 16:53:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-cmd pip install matplotlib
 
 # 計算 eGFR
 def calculate_egfr(age, creatinine, sex):
